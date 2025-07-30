a6d15cb432ef293ad3502f87e157b2c656ab0dc5
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-07-30 21:50:58 +0600

    update readme

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -37,7 +37,6 @@ export const authenticateJWT = (
   }
 };
 
-// Role based authorization middleware
 export const authorizeRoles = (...allowedRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!req.user) return res.status(401).json({ message: "Unauthorized" });
